ff3062c8c81179ad57bb164d4992867315c1d142
joe.gaudy <joe.gaudy@cs-owl.centre.edu>
2024-12-09 21:45:05 -0500

    DONE!

--- src/main/java/QTree.java
+++ src/main/java/QTree.java
@@ -2,20 +2,6 @@ import java.io.InputStream;
 import java.io.PrintStream;
 import java.util.Scanner;
 
-public class Node
-{
-	private String Q;
-	private Node N;
-	private Node Y;
-	
-	public Node(String Q)
-	{
-		this.Q = Q;
-		this.N = null;
-		this.Y = null;
-	}
-}
-
 public class QTree
 {
 	
@@ -24,91 +10,139 @@ public class QTree
 	private Node root;
 	
     //initializes the game
+	
+	public class Node
+	{
+		private Node prev;
+		private String Q;
+		private Node N;
+		private Node Y;
+		
+		public Node(String Q)
+		{
+			this.Q = Q;
+			this.N = null;
+			this.Y = null;
+		}
+	}
+	
 	public QTree(InputStream in,PrintStream out)
 	{
 		this.out=out;
 		this.in=new Scanner(in);
 		//Please initialize your data here
 		root = new Node("Is it alive?");
-		root.N = new Node("Is it a rock?");
-		root.Y = new Node("Is it a duck?");
+		root.N = new Node("Is it a Rock?");
+		root.Y = new Node("Is it a Duck?");
+		root.N.prev = root;
+		root.Y.prev = root;
+		root.prev = null;
 	}
 	
     
     //plays the game, be sure to grab input from the Scanner "in", and send your output to "out".
 	public void playGame()
 	{
-		playGame(root.N);
+		playGame(root, "");
 	}
 	
-	private void playGame(Node node)
+	private void playGame(Node node, String prevR)
 	{
-		out(node.Q);
-		R = in;
-		if (R == "Y" || R == "y")
-		{
-			out(node.Y.Q);
-		}
-		else
-		{
-			out(node.N.Q);
-		}
+		out.println(node.Q);
+		String Rprime; 
+		Rprime = in.nextLine();
 		
-		R = in;
-
-		if (R == "Y" || R == "y" && node.Y.Y == null)
+		if ((Rprime.equals("Y") || Rprime.equals("y")) && node.Y == null)
 		{				
-			out(I_WIN);
-			out(PLAY_AGAIN);
-			if (R == "Y" || R == "y" && node.Y.Y == null)
+			out.println(Strings.I_WIN);
+			out.println(Strings.PLAY_AGAIN);
+			String R = in.nextLine();
+			if (R.equals("Y") || R.equals("y"))
 			{
-				playGame(root);
+				playGame(root,""); 
+			}
+			else
+			{
+				return;
 			}
 		}
 		
-		if (R == "Y" || R == "y")
+		else if (node.Y == null)
 		{
-			playGame(node.Y);
-		}
-		
-		if (node.N.N == null)
-		{
-			out(WHAT_IS_THE_ANSWER);
-			A = in;
-			out(NEW_QUESTION + A);
-			Q = in;
-			out("Answering yes to " + Q + " means " + A + "?")
-			R = in;
-			if (R == "Y" || R == "y")
+			String [] words = node.Q.split(" ");
+			String word = words[3].substring(0,words[3].length()-1);
+			out.println(Strings.WHAT_IS_THE_ANSWER);
+			String A = in.nextLine();
+			out.println(Strings.NEW_QUESTION + word + " and a " + A);
+			String Q = in.nextLine();
+			out.println("Answering yes to " + Q + " means " + A + "?");
+			String R = in.nextLine();
+			if (R.equals("Y") || R.equals("y"))
 			{
-				Node temp = node.Y;
+				Node temp = node;
 				Node newQ = new Node(Q);
+				newQ.prev = node.prev;
 				newQ.N = temp;
-				newQ.Y = IS_IT_A + A;
-				node.Y = newQ;
-				out(PLAY_AGAIN);
-				if (R == "Y" || R == "y" && node.Y.Y == null)
+				newQ.Y = new Node(Strings.IS_IT_A + A + "?");
+				newQ.N.prev = newQ;
+				newQ.Y.prev = newQ;
+				if (prevR.toLowerCase().equals("y"))
 				{
-					playGame(root);playGame(node.Y);
+					newQ.prev.Y = newQ;
+				}
+				else
+				{
+					newQ.prev.N = newQ;
+				}
+				out.println(Strings.THANKS);
+				out.println(Strings.PLAY_AGAIN);
+				R = in.nextLine();
+				if (R.equals("Y") || R.equals("y"))
+				{
+					playGame(root,"");
+				}
+				else
+				{
+					return;
 				}
 			}
 			else
 			{
-				Node temp = node.N;
+				Node temp = node;
 				Node newQ = new Node(Q);
+				newQ.prev = node.prev;
 				newQ.Y = temp;
-				newQ.N = IS_IT_A + A;
-				node.N = newQ;
-				out(PLAY_AGAIN);
-				if (R == "Y" || R == "y" && node.Y.Y == null)
+				newQ.N = new Node(Strings.IS_IT_A + A + "?");
+				newQ.N.prev = newQ;
+				newQ.Y.prev = newQ;
+				if (prevR.toLowerCase().equals("y"))
 				{
-					playGame(root);
+					newQ.prev.Y = newQ;
+				}
+				else
+				{
+					newQ.prev.N = newQ;
+				}
+				out.println(Strings.THANKS);
+				out.println(Strings.PLAY_AGAIN);
+				R = in.nextLine();
+				if (R.equals("Y") || R.equals("y"))
+				{
+					playGame(root,"");
+				}
+				else
+				{
+					return;
 				}
 			}
 		}
+		else if (Rprime.equals("Y") || Rprime.equals("y"))
+		{
+			playGame(node.Y, Rprime);
+		}
 		else
 		{
-			playGame(node.N);
+			playGame(node.N, Rprime);
 		}
 	}
 	
